70899bd2a15b02817dba9057b05a4756d955b208
noritakasawada <snp.inc.info@gmail.com>
2024-11-17 20:53:22 +0900

    no3_commit

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     ],
     domains: [
       'images.unsplash.com',
-      'your-supabase-project.supabase.co',
+      'crgswbpgbxvtrszimnmk.supabase.co',
     ],
   },
   eslint: {
